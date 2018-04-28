88fdd0c05b7cf1c2a69ad2d3608eebcf45c4d5bf
thomas <dui1cuo@126.com>
2018-04-28 18:18:58 +0800

    fix

---
+++ AlgorithmPractiseProject/AlgorithmPractiseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlgorithmPractiseProject
+//
+//  Created by admin on 2018/4/28.
+//  Copyright © 2018年 thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlgorithmPractiseProject/AlgorithmPractiseProjectTests/AlgorithmPractiseProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  AlgorithmPractiseProjectTests.m
+//  AlgorithmPractiseProjectTests
+//
+//  Created by admin on 2018/4/28.
+//  Copyright © 2018年 thomas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlgorithmPractiseProjectTests : XCTestCase
+
+@end
+
+@implementation AlgorithmPractiseProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
